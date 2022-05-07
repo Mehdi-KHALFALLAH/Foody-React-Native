5ea46e8f5c6c75b59780f73cbba83ef8ccd0ee80
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2022-05-07 11:53:18 +0100

    fixed the layout

--- navigation/CustomDrawer.js
+++ navigation/CustomDrawer.js
@@ -17,11 +17,13 @@ import {
 } from "../constants";
 
 const Drawer = createDrawerNavigator();
+
+
 const CustomDrawerContent = ({ navigation }) => {
   return (
-    <DrawerContentScrollView scrollEnabled={true} contentContainerStyle={{ flex: 1, flexDirection : "column" }}>
-      <View>
-        <View style={{ width: "100%", alignItems: "flex-start", justifyContent: "center",flexDirection: "row"}}>
+    <DrawerContentScrollView scrollEnabled={true} contentContainerStyle={{ flex: 1 }}>
+      <View style = {{flex : 1, paddingHorizontal : SIZES.radius}}>
+        <View style={{ alignItems: "flex-start", justifyContent: "center"}}>
           <TouchableOpacity style={{ alignItems: "center", justifyContent: "center" } } onPress={() => navigation.closeDrawer()} >
             <Image source={icons.cross} style={{ height: 35, width: 35, tintColor: COLORS.white }}/>
           </TouchableOpacity>
@@ -30,11 +32,12 @@ const CustomDrawerContent = ({ navigation }) => {
           <TouchableOpacity onPress={() => console.log("profile") } style = {{}}>
             <Image  source={dummyData.myProfile?.profile_image} style={{ height: 50, width: 50, borderRadius: SIZES.radius }} />
           </TouchableOpacity>
-           <View style = {{flexDirection : "row", marginLeft : 20}}> 
-             <Text style={{ color: COLORS.white, ...FONTS.body4, width: "100%" }}>hello</Text>
+           <View style = {{marginLeft: SIZES.radius}}> 
+             
              <Text style={{ color: COLORS.white, ...FONTS.h3 }}> {dummyData.myProfile?.name} </Text>
-           </View>
+             <Text style={{ color: COLORS.white, ...FONTS.body4 }}> View your profile </Text>
         </View>
+      </View>
       </View>
     </DrawerContentScrollView>
   );
@@ -53,12 +56,12 @@ const CustomDrawer = () => {
           drawerStyle={{
           flex: 1,
           width: "65%",
-          paddingRight: 170,
+          paddingRight : 20,
           backgroundColor: "transparent",
         }}
         sceneContainerStyle={{
           backgroundColor: "transparent",
-        }}
+}}
         initialRouteName="MainLayout"
         drawerContent={props => {
           return <CustomDrawerContent navigation={props.navigation} />;
@@ -84,7 +87,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     width : '100%',
-    marginLeft : 30
+    marginLeft : 5,
+    marginTop : 15
   },
   headerName : {
 width : "100%"
